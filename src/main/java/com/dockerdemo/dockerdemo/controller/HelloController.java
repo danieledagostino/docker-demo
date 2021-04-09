c3116e493e02b3e87728cded98048699b8f9e6ec
Daniele D'Agostino <principelele@gmail.com>
2021-04-09 19:07:12 +0200

    first commit

---
+++ src/main/java/com/dockerdemo/dockerdemo/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.dockerdemo.dockerdemo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("")
+public class HelloController {
+
+
+	@GetMapping(value = "/hi/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+	public ResponseEntity<String> hi(@PathVariable("name") String name) {
+		
+        return new ResponseEntity<>("Hi "+name+"!!", HttpStatus.OK);
+	}
+
+}

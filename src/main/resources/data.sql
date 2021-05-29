20b0ae8d886d086cb5499e51af5c3e5ce235cd16
daniele.dagostino <daniele.dagostino@accenture.com>
2021-05-29 13:29:54 +0200

    script changed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,9 @@
-INSERT INTO PERSON VALUES(1, 'test1@pincio.it', 'Daniele1', '$2a$10$HKL2IWagq68w4WiWwmKyp.N9RumfjlOgedCnjfYP20/sWG6/Mh/8W', null, 'USER', 'Test', null, true);
-INSERT INTO PERSON VALUES(2, 'test2@pincio.it', 'Daniele2', '$2a$10$HKL2IWagq68w4WiWwmKyp.N9RumfjlOgedCnjfYP20/sWG6/Mh/8W', null, 'USER', 'Test', null, true);
-INSERT INTO PERSON VALUES(3, 'test3@pincio.it', 'Daniele3', '$2a$10$HKL2IWagq68w4WiWwmKyp.N9RumfjlOgedCnjfYP20/sWG6/Mh/8W', null, 'USER', 'Test', null, true);
-INSERT INTO PERSON VALUES(4, 'test4@pincio.it', 'Daniele4', '$2a$10$HKL2IWagq68w4WiWwmKyp.N9RumfjlOgedCnjfYP20/sWG6/Mh/8W', null, 'USER', 'Test', null, true);
-INSERT INTO PERSON VALUES(5, 'test5@pincio.it', 'Daniele5', '$2a$10$HKL2IWagq68w4WiWwmKyp.N9RumfjlOgedCnjfYP20/sWG6/Mh/8W', null, 'USER', 'Test', null, true);
+INSERT INTO PERSON('email', 'name','password','photo_id','role','surname','token','valid') VALUES
+('test1@pincio.it', 'Daniele1', '$2a$10$HKL2IWagq68w4WiWwmKyp.N9RumfjlOgedCnjfYP20/sWG6/Mh/8W', null, 'USER', 'Test', null, true),
+('test2@pincio.it', 'Daniele2', '$2a$10$HKL2IWagq68w4WiWwmKyp.N9RumfjlOgedCnjfYP20/sWG6/Mh/8W', null, 'USER', 'Test', null, true),
+('test3@pincio.it', 'Daniele3', '$2a$10$HKL2IWagq68w4WiWwmKyp.N9RumfjlOgedCnjfYP20/sWG6/Mh/8W', null, 'USER', 'Test', null, true),
+('test4@pincio.it', 'Daniele4', '$2a$10$HKL2IWagq68w4WiWwmKyp.N9RumfjlOgedCnjfYP20/sWG6/Mh/8W', null, 'USER', 'Test', null, true),
+('test5@pincio.it', 'Daniele5', '$2a$10$HKL2IWagq68w4WiWwmKyp.N9RumfjlOgedCnjfYP20/sWG6/Mh/8W', null, 'USER', 'Test', null, true);
 
 INSERT INTO RACE_TYPE VALUES(1, 'Skates', 25);
 INSERT INTO RACE_TYPE VALUES(2, 'Bike', 10);

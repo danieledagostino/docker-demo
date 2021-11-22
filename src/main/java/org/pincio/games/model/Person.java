5fd3cf54fc58433c75bc0c1f55ca644cb4edf7dd
daniele.dagostino <daniele.dagostino@accenture.com>
2021-11-22 16:20:38 +0100

    person model id generated value strategy fix

--- src/main/java/org/pincio/games/model/Person.java
+++ src/main/java/org/pincio/games/model/Person.java
@@ -10,7 +10,7 @@ import java.util.Set;
 public class Person {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     /*
     @GeneratedValue(generator = "person-sequence-generator")
     @GenericGenerator(
